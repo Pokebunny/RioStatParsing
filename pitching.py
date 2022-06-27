c55975e432b1250a609e4c98ae9c6371441eaa33
thknkg <108246578+thknkg@users.noreply.github.com>
2022-06-26 21:30:20 -0500

    Update pitching.py
    only did opponent batting avg, ERA, K%, and ERA-. thinking about doing + stats for avg, k%, etc. as well, and maybe doing an aggregate + stat

--- pitching.py
+++ pitching.py
@@ -15,4 +15,53 @@ elif stars == 1:
 if ranked == 1:
     url += "&tag=Ranked"
 
-print("Hello World")
+all_response = requests.get(url).json()
+all_char_response = requests.get(url + "&by_char=1").json()
+
+if user != "":
+    url += "&username=" + user
+
+response = requests.get(url).json()
+
+stats = response["Stats"]["Pitching"]
+d_avg = stats["hits_allowed"] / (stats["batters_faced"]-stats["walks_bb"]-stats["walks_hbp"])
+era = 9 * stats["runs_allowed"] / (stats["outs_pitched"] / 3)
+kp = (stats["strikeouts_pitched"] / stats["batters_faced"])*100
+
+overall = all_response["Stats"]["Pitching"]
+overall_davg = overall["hits_allowed"] / (overall["batters_faced"] - overall["walks_bb"] - overall["walks_hbp"])
+overall_era = 9 * overall["runs_allowed"] / (overall["outs_pitched"] / 3)
+overall_kp = (stats["strikeouts_pitched"] / stats["batters_faced"])*100
+era_minus = ((era / overall_era)) * 100
+
+print("opAVG / ERA / K%")
+print("OVERALL: " + "{:.3f}".format(d_avg) + " / " + "{:.2f}".format(era) + " / " + "{:.1f}".format(kp)+"%" + " / " + "{:.0f}".format(era_minus) + " oERA+")
+
+url += "&by_char=1"
+
+response = requests.get(url).json()
+sorted_char_list = []
+try:
+    sorted_char_list = sorted(response["Stats"].keys(), key=lambda x: response["Stats"][x]["Pitching"]["batters_faced"], reverse=True)
+except KeyError:
+    print("There was an error sorting the character list")
+    sorted_char_list = sorted(response["Stats"].keys())
+
+for char in sorted_char_list:
+    char_stats = response["Stats"][char]["Pitching"]
+    if char_stats["batters_faced"] > 0 and char_stats["outs_pitched"] > 0:
+        d_avg = char_stats["hits_allowed"] / (char_stats["batters_faced"] - char_stats["walks_bb"] - char_stats["walks_hbp"])
+        era = 9 * char_stats["runs_allowed"] / (char_stats["outs_pitched"] / 3)
+        kp = (char_stats["strikeouts_pitched"] / char_stats["batters_faced"])*100
+
+
+
+
+        overall = all_char_response["Stats"][char]["Pitching"]
+        if user == "":
+            overall = all_response["Stats"]["Pitching"]
+        overall_davg = overall["hits_allowed"] / (overall["batters_faced"] - overall["walks_bb"] - overall["walks_hbp"])
+        overall_era = 9 * overall["runs_allowed"] / (overall["outs_pitched"] / 3)
+        overall_kp = (overall["strikeouts_pitched"] / overall["batters_faced"])*100
+        era_minus = ((era / overall_era)) * 100
+        print(char + " / " + "{:.0f}".format(char_stats["batters_faced"]) + " batter(s) faced" + " / " + "{:.3f}".format(d_avg) + " / " + "{:.2f}".format(era) + " ERA " " / " + "{:.1f}".format(kp) + "%" + " / " + str(round((era_minus))) + " oERA-")
