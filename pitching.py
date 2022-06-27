dee6a1a3ea734c1aab2b3f660ef8ca18874b2a3c
Pokebunny <pokebunnytaber@gmail.com>
2022-06-26 22:00:56 -0400

    Create pitching.py

---
+++ pitching.py
@@ -0,0 +1,16 @@
+import requests
+
+print("Enter username of player (blank for all stats):")
+user = input()
+print("Enter '0' for stars-off data; '1' for stars-on")
+stars = int(input())
+print("Enter '0' for all data, '1' for ranked only")
+ranked = int(input())
+
+url = "https://projectrio-api-1.api.projectrio.app/detailed_stats/?exclude_batting=1&exclude_fielding=1&exclude_misc=1"
+if stars == 0:
+    url += "&tag=Normal"
+elif stars == 1:
+    url += "&tag=Superstar"
+if ranked == 1:
+    url += "&tag=Ranked"
