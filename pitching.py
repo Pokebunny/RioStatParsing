c9bcb368b2cd0b9c090add4aaaeb836c363094af
Pokebunny <pokebunnytaber@gmail.com>
2022-06-28 18:32:35 -0400

    cleanup

--- pitching.py
+++ pitching.py
@@ -31,17 +31,17 @@ era = 9 * stats["runs_allowed"] / (stats["outs_pitched"] / 3)
 kp = (stats["strikeouts_pitched"] / stats["batters_faced"])*100
 
 overall = all_response["Stats"]["Pitching"]
-overall_davg = overall["hits_allowed"] / (overall["batters_faced"] - overall["walks_bb"] - overall["walks_hbp"])
+overall_avg = overall["hits_allowed"] / (overall["batters_faced"] - overall["walks_bb"] - overall["walks_hbp"])
 overall_era = 9 * overall["runs_allowed"] / (overall["outs_pitched"] / 3)
 overall_kp = (stats["strikeouts_pitched"] / stats["batters_faced"])*100
 # character ERA-
-cera_minus = ((era / overall_era)) * 100
+cera_minus = (era / overall_era) * 100
 
 print("opAVG / ERA / K%")
 
 # not sure if overall is cERA or ERA, I assume the latter though
 if user != "":
-    print("OVERALL: " + "{:.3f}".format(d_avg) + " / " + "{:.2f}".format(era) + " / " + "{:.1f}".format(kp)+"%" + " / " + "{:.0f}".format(cera_minus) + " cERA-")
+    print("OVERALL: " + "{:.3f}".format(d_avg) + " / " + "{:.2f}".format(era) + " / " + "{:.1f}".format(kp)+"%" + " / " + "{:.0f}".format(cera_minus) + " ERA-")
 else:
     print("OVERALL: " + "{:.3f}".format(d_avg) + " / " + "{:.2f}".format(era) + " / " + "{:.1f}".format(kp) + "%" + " / " + "{:.0f}".format(cera_minus) + " ERA-")
 url += "&by_char=1"
@@ -61,15 +61,13 @@ for char in sorted_char_list:
         era = 9 * char_stats["runs_allowed"] / (char_stats["outs_pitched"] / 3)
         kp = (char_stats["strikeouts_pitched"] / char_stats["batters_faced"])*100
 
-
-
         char_or_all = " cERA-"
         overall = all_char_response["Stats"][char]["Pitching"]
         if user == "":
             overall = all_response["Stats"]["Pitching"]
             char_or_all = " ERA-"
-        overall_davg = overall["hits_allowed"] / (overall["batters_faced"] - overall["walks_bb"] - overall["walks_hbp"])
+        overall_avg = overall["hits_allowed"] / (overall["batters_faced"] - overall["walks_bb"] - overall["walks_hbp"])
         overall_era = 9 * overall["runs_allowed"] / (overall["outs_pitched"] / 3)
         overall_kp = (overall["strikeouts_pitched"] / overall["batters_faced"])*100
-        cera_minus = ((era / overall_era)) * 100
+        cera_minus = (era / overall_era) * 100
         print(char + " / " + "{:.0f}".format(char_stats["batters_faced"]) + " batter(s) faced" + " / " + "{:.3f}".format(d_avg) + " / " + "{:.2f}".format(era) + " ERA " " / " + "{:.1f}".format(kp) + "%" + " / " + str(round((cera_minus))) + char_or_all)
